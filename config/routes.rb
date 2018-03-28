992f388c8485fdea622ef035a234fd0d76556836
Parth Sharma <parth.sharma@trios.com>
2018-03-27 18:07:42 -0700

    Basic login registration with bootstrap

---
+++ config/routes.rb
@@ -0,0 +1,8 @@
+Rails.application.routes.draw do
+  devise_for :users
+  get 'home/front'
+
+  root to: "home#front"
+
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
